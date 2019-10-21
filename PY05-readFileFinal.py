7f9f73bb9545ef1d0a8787ba49dc4e3c5093d096
NigelSlack <47183224+NigelSlack@users.noreply.github.com>
2019-10-21 11:24:26 +0100

    Week03 labs

--- PY05-readFileFinal.py
+++ PY05-readFileFinal.py
@@ -1,12 +1,15 @@
 from bs4 import BeautifulSoup
+import csv
 with open("C:/Users/dingl/Documents/Data Representation/carviewer.html") as fp:
  soup = BeautifulSoup(fp,'html.parser')
+#print (soup.tr)
+employee_file = open('week02data.csv', mode='w')
+employee_writer = csv.writer(employee_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 rows = soup.findAll("tr")
 for row in rows:
- #print(row)
  cols = row.findAll("td")
  dataList = []
  for col in cols:
    dataList.append(col.text)
- print (dataList)
-
+   employee_writer.writerow(dataList)
+employee_file.close()
